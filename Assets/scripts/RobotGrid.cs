cd2a21368156cd8ae94d0f7213a5c2f625ed7dc4
poemdexter <poemdexter@gmail.com>
2016-04-16 13:50:32 -0500

    added new grid commands

--- Assets/scripts/RobotGrid.cs
+++ Assets/scripts/RobotGrid.cs
@@ -1,13 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class RobotGrid : MonoBehaviour
 {
     public int width = 4;
     public int height = 4;
 
-    void Update()
-    {
+    private Queue<string> commandQueue;
 
+    void Start()
+    {
+        commandQueue = new Queue<string>();
+    }
+
+    public void NewInput(string command)
+    {
+        commandQueue.Enqueue(command);
+    }
+
+    public void ClearQueue()
+    {
+        commandQueue.Clear();
+    }
+
+    public void Execute()
+    {
+        foreach(string command in commandQueue)
+        {
+            // robot do thing
+        }
     }
 }
