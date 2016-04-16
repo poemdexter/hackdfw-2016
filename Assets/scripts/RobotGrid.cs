83b5e7ed5936883831f75052224b5a90476a8d7b
poemdexter <poemdexter@gmail.com>
2016-04-16 13:39:02 -0500

    fixed space skybox added base RobotGrid script

---
+++ Assets/scripts/RobotGrid.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotGrid : MonoBehaviour
+{
+    public int width = 4;
+    public int height = 4;
+
+    void Update()
+    {
+
+    }
+}
